64fd9134b4a49878fbc46ebcea044f0918ac1481
Dylan Renwick <skidsirongenesisdev@gmail.com>
2019-04-15 08:32:28 -0400

    Added all the things

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,28 @@
-import * as fs from "fs";
-
-import { Compiler } from "./Compiler";
+import { readFileSync } from "fs";
+import { AbstractSyntaxTree } from "./AST/AbstractSyntaxTree";
+import { Parser } from "./Parser";
+import { Token } from "./Token";
 import { Tokenizer } from "./Tokenizer";
 
 let sourceFile: string = "main.ns";
 if (process.argv.length > 2) {
     sourceFile = process.argv[2];
 }
-let fileBuffer: Buffer = fs.readFileSync(sourceFile);
+let fileBuffer: Buffer = readFileSync(sourceFile);
 
 let outFile: string = "main.asm";
 if (process.argv.length > 3) {
     outFile = process.argv[3];
 }
-let lines: string[] = fileBuffer.toString("utf8").split("\n");
-lines.forEach(line => Tokenizer.Tokenize(line));
+let code: string = fileBuffer.toString("utf8");
+let tokens: Token[] = Tokenizer.tokenize(code);
 
 console.log("\nSource NuStack:");
 console.log(fileBuffer.toString("utf8"));
 console.log("\nTokens:");
-console.log(JSON.stringify(Tokenizer.Tokens));
+console.log(JSON.stringify(tokens.map(tok => tok.toString())));
 
-let tokens: string[] = Tokenizer.Tokens;
+let ast: AbstractSyntaxTree = Parser.Parse(tokens);
 
-Compiler.Compile(tokens);
+console.log("\nAST:");
+console.log(JSON.stringify(ast, null, 2));
