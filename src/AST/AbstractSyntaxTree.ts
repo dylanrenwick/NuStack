c3480b943aa4d3d395a67564ddf020a13f4fc1f8
Dylan Renwick <skidsirongenesisdev@gmail.com>
2019-04-23 13:34:52 -0400

    Added input validation to AbstractSyntaxTree ctor

--- src/AST/AbstractSyntaxTree.ts
+++ src/AST/AbstractSyntaxTree.ts
@@ -4,6 +4,9 @@ export class AbstractSyntaxTree {
     public root: ProgramASTNode;
 
     public constructor(rootNode: ProgramASTNode) {
+        if (!(rootNode instanceof ProgramASTNode)) {
+            throw new Error("Invalid root node");
+        }
         this.root = rootNode;
     }
 }
