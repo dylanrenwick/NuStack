4f0816343ea1c737ede825882fe23dc96fb0140c
Dylan Renwick <skidsirongenesisdev@gmail.com>
2019-04-23 16:23:21 -0400

    Added applyOperator function

--- src/AST/OperationASTNode.ts
+++ src/AST/OperationASTNode.ts
@@ -29,3 +29,22 @@ export enum OperationType {
     LogicalOR,
     LogicalAND
 }
+
+export function applyOperator(op: OperationType, operands: number[]): number {
+    switch (op) {
+        case OperationType.Negation: return -operands[0];
+        case OperationType.BitwiseNOT: return ~operands[0];
+        case OperationType.LogicalNOT: return operands[0] === 0 ? 0 : 1;
+        case OperationType.Addition: return operands[0] + operands[1];
+        case OperationType.Subtraction: return operands[0] - operands[1];
+        case OperationType.Multiplication: return operands[0] * operands[1];
+        case OperationType.Division: return operands[0] / operands[1];
+        case OperationType.LessThan: return operands[0] < operands[1];
+        case OperationType.MoreThan: return operands[0] > operands[1];
+        case OperationType.Equal: return operands[0] === operands[1];
+        case OperationType.NotEqual: return operands[0] !== operands[1];
+        case OperationType.LessThanEqual: return operands[0] <= operands[1];
+        case OperationType.MoreThanEqual: return operands[0] >= operands[1];
+        default: return null
+    }
+}
