ecf019fdd339510c3e4eed73428945ffa38431c4
Dylan Renwick <skidsirongenesisdev@gmail.com>
2019-04-24 10:10:28 -0400

    Cleaned up and fixed DiadicASTNode tests

--- test/AST/DiadicASTNode.spec.ts
+++ test/AST/DiadicASTNode.spec.ts
@@ -2,15 +2,17 @@ import { expect } from "chai";
 import { ConstantASTNode } from "../../src/AST/ConstantASTNode";
 import { DiadicASTNode } from "../../src/AST/DiadicASTNode";
 import { ExpressionASTNode } from "../../src/AST/ExpressionASTNode";
-import { OperationType } from "../../src/AST/OperationASTNode";
+import { applyOperator, OperationType } from "../../src/AST/OperationASTNode";
 
 function forOpTypes(callback: (type: OperationType, ...params: any[]) => void, ...params: any[] ) {
     let monadicTypes = [
         OperationType.BitwiseNOT, OperationType.LogicalNOT, OperationType.Negation
     ];
 
-    for (let type of monadicTypes) {
-        callback(type, ...params);
+    for (let type in OperationType) {
+        if (!/^[0-9]+$/.test(type)) continue;
+        if (monadicTypes.includes(parseInt(type))) continue;
+        callback(parseInt(type), ...params);
     }
 }
 
@@ -33,52 +35,45 @@ describe("DiadicASTNode", () => {
     describe("DiadicASTNode.childNodes", () => {
         it("should return an array containing operands", () => {
             let node = new DiadicASTNode(0, null, null);
-            expect(node.childNodes).to.equal([null, null]);
+            expect(node.childNodes).to.deep.equal([null, null]);
             let innerNode = new ConstantASTNode(0);
             node = new DiadicASTNode(0, innerNode, null);
-            expect(node.childNodes).to.equal([ innerNode, null]);
+            expect(node.childNodes).to.deep.equal([ innerNode, null]);
             node = new DiadicASTNode(0, null, innerNode);
-            expect(node.childNodes).to.equal([ null, innerNode]);
+            expect(node.childNodes).to.deep.equal([ null, innerNode]);
         });
     });
 
     describe("DiadicASTNode.expressionValue", () => {
         it("should correctly apply an operator to a constant value", () => {
-            for (let i = 0; i < 10; i++) {
-                let expected: number[] = [];
-                expected[OperationType.Negation] = -i;
-                expected[OperationType.BitwiseNOT] = ~i;
-                expected[OperationType.LogicalNOT] = i === 0 ? 0 : 1;
-
-                forOpTypes((type: OperationType, expectVals: number[]) => {
-                    expect(new DiadicASTNode(type, new ConstantASTNode(i)).expressionValue).to.equal(expectVals[type]);
-                }, expected);
-            }
+            forOpTypes((type: OperationType) => {
+                let a = Math.floor(Math.random() * 15000);
+                let b = Math.floor(Math.random() * 15000);
+                expect(new DiadicASTNode(type, new ConstantASTNode(a), new ConstantASTNode(b)).expressionValue)
+                    .to.equal(applyOperator(type, [a, b]));
+            });
         });
 
         it("should correctly apply an operator to a child with an expressionValue", () => {
-            for (let i = 0; i < 10; i++) {
-                let expected: number[] = [];
-                expected[OperationType.Negation] = -(-i);
-                expected[OperationType.BitwiseNOT] = ~(~i);
-                expected[OperationType.LogicalNOT] = (i === 0 ? 0 : 1) === 0 ? 0 : 1;
-
-                forOpTypes((type: OperationType, expectVals: number[]) => {
-                    expect(new DiadicASTNode(
-                        type, new DiadicASTNode(type, new ConstantASTNode(i))
-                    ).expressionValue).to.equal(expectVals[type]);
-                }, expected);
-            }
+            forOpTypes((type: OperationType) => {
+                let a = Math.floor(Math.random() * 15000);
+                let b = Math.floor(Math.random() * 15000);
+                let expected = applyOperator(type, [applyOperator(type, [a, 0]), b]);
+                expect(new DiadicASTNode(
+                    type, new DiadicASTNode(type, new ConstantASTNode(a), new ConstantASTNode(0)),
+                    new ConstantASTNode(b)
+                ).expressionValue).to.equal(expected);
+            });
         });
 
         it("should return null with a non-constant, null-expressionValue child", () => {
             class TestNode extends ExpressionASTNode { get expressionValue(): any { return null; } }
 
-            for (let i = 0; i < 10; i++) {
-                forOpTypes((type: OperationType) => {
-                    expect(new DiadicASTNode(type, new TestNode()).expressionValue).to.equal(null);
-                });
-            }
+            forOpTypes((type: OperationType) => {
+                expect(new DiadicASTNode(
+                    type, new TestNode(), new ConstantASTNode(Math.floor(Math.random() * 15000))
+                ).expressionValue).to.equal(null);
+            });
         });
     });
 });
