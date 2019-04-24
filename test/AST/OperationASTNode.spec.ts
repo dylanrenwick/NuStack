fe0f59788836469a87c6a43dc6da8c6bee73f5da
Dylan Renwick <skidsirongenesisdev@gmail.com>
2019-04-24 10:37:51 -0400

    Added applyOperator tests

--- test/AST/OperationASTNode.spec.ts
+++ test/AST/OperationASTNode.spec.ts
@@ -31,4 +31,51 @@ describe("OperationASTNode", () => {
             });
         });
     });
+
+    describe("OperationASTNode.applyOperator()", () => {
+        it("should correctly evaluate operations", () => {
+            forOpTypes((type: OperationType) => {
+                let expected = null;
+                let a = Math.floor(Math.random() * 15000);
+                let b = Math.floor(Math.random() * 15000);
+                switch (type) {
+                    case OperationType.Negation: expected = -a; break;
+                    case OperationType.BitwiseNOT: expected = ~a; break;
+                    case OperationType.LogicalNOT: expected = a === 0 ? 0 : 1; break;
+                    case OperationType.Addition: expected = a + b; break;
+                    case OperationType.Subtraction: expected = a - b; break;
+                    case OperationType.Multiplication: expected = a * b; break;
+                    case OperationType.Division: expected = a / b; break;
+                    case OperationType.LessThan: expected = a < b; break;
+                    case OperationType.MoreThan: expected = a > b; break;
+                    case OperationType.Equal: expected = a === b; break;
+                    case OperationType.NotEqual: expected = a !== b; break;
+                    case OperationType.MoreThanEqual: expected = a >= b; break;
+                    case OperationType.LessThanEqual: expected = a <= b; break;
+                    case OperationType.LogicalOR: expected = a | b; break;
+                    case OperationType.LogicalAND: expected = a & b; break;
+                }
+
+                if (typeof(expected) === "boolean") expected = expected ? 1 : 0;
+                if (Number.isNaN(expected)) expected = null;
+
+                console.log(type, a, b, expected);
+
+                expect(OperationASTNode.applyOperator(type, [a, b])).to.equal(expected);
+            });
+        });
+
+        it("should correctly evaluate logicalNOT", () => {
+            expect(OperationASTNode.applyOperator(OperationType.LogicalNOT, [14])).to.equal(1);
+            expect(OperationASTNode.applyOperator(OperationType.LogicalNOT, [1])).to.equal(1);
+            expect(OperationASTNode.applyOperator(OperationType.LogicalNOT, [-24])).to.equal(1);
+            expect(OperationASTNode.applyOperator(OperationType.LogicalNOT, [-1])).to.equal(1);
+            expect(OperationASTNode.applyOperator(OperationType.LogicalNOT, [0])).to.equal(0);
+        });
+
+        it("should return null on NaN operations", () => {
+            // 0 / 0 === NaN
+            expect(OperationASTNode.applyOperator(OperationType.Division, [0, 0])).to.be.null;
+        });
+    });
 });
