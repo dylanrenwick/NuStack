0f1705c8d7d599dc6e4719034ed0ade49bbdca5e
Dylan Renwick <skidsirongenesisdev@gmail.com>
2019-04-26 11:27:50 -0400

    Added public access members to AssignmentASTNode

--- src/AST/AssignmentASTNode.ts
+++ src/AST/AssignmentASTNode.ts
@@ -5,6 +5,13 @@ import { OperationType } from "./OperationASTNode";
 import { VariableASTNode } from "./VariableASTNode";
 
 export class AssignmentASTNode extends DiadicASTNode {
+    public get declaration(): Declaration {
+        return (this.childNodes[0] as VariableASTNode).declaration;
+    }
+    public get expression(): ExpressionASTNode {
+        return this.childNodes[1];
+    }
+
     public constructor(dec: Declaration, expr: ExpressionASTNode) {
         super(OperationType.Assignment, new VariableASTNode(dec), expr);
     }
