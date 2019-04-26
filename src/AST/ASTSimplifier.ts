f182e7f2d4fd900269915a65bbb1c2b351bc23b7
Dylan Renwick <skidsirongenesisdev@gmail.com>
2019-04-26 11:28:08 -0400

    Began implementing basic variable checks in ASTSimplifier

--- src/AST/ASTSimplifier.ts
+++ src/AST/ASTSimplifier.ts
@@ -1,5 +1,9 @@
+import { Declaration } from "../Declaration";
+import { HashMap } from "../HashMap";
 import { AbstractSyntaxTree } from "./AbstractSyntaxTree";
+import { AssignmentASTNode } from "./AssignmentASTNode";
 import { ConstantASTNode } from "./ConstantASTNode";
+import { DeclarationASTNode } from "./DeclarationASTNode";
 import { ExpressionASTNode } from "./ExpressionASTNode";
 import { ProgramASTNode } from "./ProgramASTNode";
 import { ReturnStatementASTNode } from "./ReturnStatementASTNode";
@@ -7,6 +11,8 @@ import { StatementASTNode } from "./StatementASTNode";
 import { SubroutineASTNode } from "./SubroutineASTNode";
 
 export class ASTSimplifier {
+    private static variables: HashMap<string, Declaration> = new HashMap<string, Declaration>();
+
     public static SimplifyTree(ast: AbstractSyntaxTree): AbstractSyntaxTree {
         ast.root = new ProgramASTNode(this.simplifySubroutine(ast.root.childNodes));
         return ast;
@@ -22,6 +28,23 @@ export class ASTSimplifier {
     private static simplifyStatement(statement: StatementASTNode): StatementASTNode {
         if (statement instanceof ReturnStatementASTNode) {
             return new ReturnStatementASTNode(this.simplifyExpression(statement.childNodes));
+        } else if (statement instanceof DeclarationASTNode) {
+            let dec: Declaration = statement.declaration;
+            if (this.variables.Has(dec.variableName)) {
+                throw new Error("Attempt to declare varable '"
+                    + dec.variableName + "' [" + dec.variableType
+                    + "] but variable was already declared");
+            }
+            dec.addValue(null);
+            this.variables.Add(dec.variableName, dec);
+        } else if (statement instanceof AssignmentASTNode) {
+            let dec: Declaration = statement.declaration;
+            if (statement.expression.expressionValue !== null) {
+                dec.addValue(statement.expression.expressionValue);
+                statement = new AssignmentASTNode(dec,
+                    this.simplifyExpression(statement.expression)
+                );
+            }
         }
 
         return statement;
