c5c7c03da81b0a8e71f8ab58c27234c0c53c72da
Dylan Renwick <drenwick@verticalsoftware.ca>
2019-05-08 15:01:55 -0400

    Added architecture to args input

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { NuStack } from "./NuStack";
 
 option("input-file", "The source file from which to read code", "main.ns");
 option("output-file", "The file to write compiled ASM to", "main.asm");
+option("arch", "The architecture to target", 32);
 option("debug", "Display debug information during compile");
 option("optimization-level", "The level at which to apply optimization to the AST", 0);
 
@@ -11,6 +12,7 @@ const args = parse(process.argv);
 
 let sourceFile = args.i;
 let outFile = args.o;
+let arch = args.a.toString();
 let debug = args.d;
 let o = args.O;
 
@@ -18,6 +20,6 @@ let fileBuffer: Buffer = readFileSync(sourceFile);
 
 let code: string = fileBuffer.toString("utf8");
 
-let asm: string = NuStack.compile(code, debug, o);
+let asm: string = NuStack.compile(code, arch, debug, o);
 
 writeFileSync(outFile, asm);
