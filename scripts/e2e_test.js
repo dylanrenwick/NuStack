b3397c52c7d8b80fed31d41310a47dc7689b4634
Dylan Renwick <drenwick@verticalsoftware.ca>
2019-05-08 16:25:26 -0400

    Cleaned up e2e tests

--- scripts/e2e_test.js
+++ scripts/e2e_test.js
@@ -9,28 +9,30 @@ if (!fs.existsSync("examples/tests.json")) {
 const tests = require("../examples/tests.json");
 
 for (let test of tests) {
-    for (let i = 0; i < 3; i++) {
-        let testName = test.fileName;
-        let expected = test.answer;
+    let testName = test.fileName;
+    let expected = test.answer;
 
-        exec("node bin/index.js -i examples/" + testName + " -o examples/" + testName.split(".")[0] + ".asm -O " + i
-            + " && nasm -f elf64 -o examples/" + testName.split(".")[0] + ".o examples/" + testName.split(".")[0] + ".asm"
-            + " && ld -e main -o examples/" + testName.split(".")[0] + " examples/" + testName.split(".")[0] + ".o",
+    for (let i = 0; i < 3; i++) {
+        let realName = testName + "-" + i;
+
+        exec("node bin/index.js -i examples/" + testName + ".ns -o examples/" + realName + ".asm -a 64 -O " + i
+            + " && nasm -f elf64 -o examples/" + realName + ".o examples/" + realName + ".asm"
+            + " && ld -e main -o examples/" + realName + " examples/" + realName + ".o",
         (err, stdout, stderr) => {
             if (err) {
                 console.log(testName + " - FAIL");
-                console.log("Expected " + expected + " but got " + outCode);
                 console.log(stdout);
                 console.log(stderr);
             }
         });
-        exec("./examples/" + testName.split(".")[0], (err, stdout, stderr) => {
+        exec("./examples/" + testName, (err, stdout, stderr) => {
             let outCode = 0;
             if (err) outCode = err.code;
 
             console.log(outCode, expected);
 
             if (outCode !== expected) {
+                console.log("Expected " + expected + " but got " + outCode);
                 console.log(stdout);
                 console.log(stderr);
             }
