1968544fd963210881a31b80fa7f58cc46458ab0
dylanrenwick <skidsirongenesisdev@gmail.com>
2019-05-14 11:04:22 -0400

    Added before and after sections to LoopASTNode
    Allows possible for loops in the future

--- src/AST/LoopASTNode.ts
+++ src/AST/LoopASTNode.ts
@@ -3,16 +3,26 @@ import { ExpressionASTNode } from "./ExpressionASTNode";
 import { StatementASTNode } from "./StatementASTNode";
 
 export class LoopASTNode extends StatementASTNode {
+    private before: StatementASTNode[];
+    private after: StatementASTNode[];
     private cond: ExpressionASTNode;
     private children: StatementASTNode[];
 
     public get condition(): ExpressionASTNode { return this.cond; }
-    public get childNodes(): StatementASTNode[] { return this.children; }
+    public get beforeNodes(): StatementASTNode[] { return this.before; }
+    public get afterNodes(): StatementASTNode[] { return this.after; }
+    public get childNodes(): StatementASTNode[] {
+        return this.children.concat(this.after !== undefined ? this.after : []);
+    }
 
-    public constructor(cond: ExpressionASTNode, children: StatementASTNode[]) {
+    public constructor(cond: ExpressionASTNode, children: StatementASTNode[],
+                       before?: StatementASTNode[], after?: StatementASTNode[]
+    ) {
         super();
         this.cond = cond;
         this.children = children;
+        this.before = before;
+        this.after = after;
     }
 
     public toString(sb: StringBuilder): StringBuilder {
