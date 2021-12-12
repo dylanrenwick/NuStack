d5b86f26c0dd3f83ee296fb4de2108a0eb523e13
Dylan Renwick <skidsirongenesisdev@gmail.com>
2021-12-12 00:13:27 -0500

    Re-added ExpectedTokenException

--- NuStack.Core/Tokens/TokenExceptions.cs
+++ NuStack.Core/Tokens/TokenExceptions.cs
@@ -1,5 +1,13 @@
 ﻿namespace NuStack.Core.Tokens
 {
+    public class ExpectedTokenException : Exception
+    {
+        private const string errorFormat = "Expected token '{0}' but found '{1}' at Line: {2}, col {3}";
+
+        public ExpectedTokenException(TokenType expected, TokenType found, int lineNo, int colNo)
+            : base(string.Format(errorFormat, expected, found, lineNo, colNo)) { }
+    }
+
     public class InvalidTokenException: Exception
     {
         private const string errorFormat = "Could not tokenize character '{0}' at Line {1}, col {2}";
