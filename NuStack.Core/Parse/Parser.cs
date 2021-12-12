1b0a2c3a4055ad8a1081aaded423c84ea8386152
Dylan Renwick <skidsirongenesisdev@gmail.com>
2021-12-12 01:21:35 -0500

    Implemented entry point detection in Parser

--- NuStack.Core/Parse/Parser.cs
+++ NuStack.Core/Parse/Parser.cs
@@ -6,28 +6,46 @@ namespace NuStack.Core.Parse
     public class Parser
     {
         private TokenStream tokenStream;
+        private NameResolver nameResolver;
+
+        private FuncFingerprint entryPoint;
+
+        public Parser()
+        {
+            nameResolver = new NameResolver();
+        }
 
         public ASTNode Parse(TokenStream tokens)
         {
             tokenStream = tokens;
+            nameResolver.Clear();
+
+            findFuncDefinitions();
+            if (!nameResolver.TryResolve("main", out entryPoint))
+                throw new ParserException("Could not find entry point method with name 'main'");
+
             return null;
         }
 
-        private IEnumerable<int> findFuncDefinitions()
+        private void findFuncDefinitions()
         {
-            var result = new List<int>();
             tokenStream.EachToken(TokenType.Keyword, "fn");
             while (tokenStream.MoveNext())
             {
-                int fnStart = tokenStream.CurrentIndex;
-                Token ident = tokenStream.Expect(TokenType.Identifier);
-                string funcName = ident.Value;
-                tokenStream.Expect(TokenType.OpenParen);
-                tokenStream.Expect(TokenType.CloseParen);
-                result.Add(fnStart);
+                parseFuncFingerprint();
             }
+        }
 
-            return result;
+        private FuncFingerprint parseFuncFingerprint()
+        {
+            int fnStart = tokenStream.CurrentIndex;
+            Token ident = tokenStream.Expect(TokenType.Identifier);
+            string funcName = ident.Value;
+
+            tokenStream.Expect(TokenType.OpenParen);
+            tokenStream.Expect(TokenType.CloseParen);
+
+            return nameResolver.RegisterFunctionFingerprint(fnStart, fnStart + 3, funcName);
         }
     }
 }
