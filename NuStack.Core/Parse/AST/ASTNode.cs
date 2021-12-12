43987b5cc6ad135f09410976df6d4f6361258a83
Dylan Renwick <skidsirongenesisdev@gmail.com>
2021-12-11 22:30:39 -0500

    Added ASTNode

---
+++ NuStack.Core/Parse/AST/ASTNode.cs
@@ -0,0 +1,16 @@
+﻿namespace NuStack.Core.Parse.AST
+{
+    internal abstract class ASTNode
+    {
+        public abstract IEnumerable<ASTNode> Children { get; }
+
+        public abstract void WriteToStringBuilder(StringBuilder builder);
+
+        public override string ToString()
+        {
+            var builder = new StringBuilder();
+            WriteToStringBuilder(builder);
+            return builder.ToString();
+        }
+    }
+}
