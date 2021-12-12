25fababbd6b0131d9b4d76b1217beb4b9a70e2e1
Dylan Renwick <skidsirongenesisdev@gmail.com>
2021-12-12 00:14:01 -0500

    Added Parser

---
+++ NuStack.Core/Parse/Parser.cs
@@ -0,0 +1,16 @@
+﻿using NuStack.Core.Tokens;
+using NuStack.Core.Parse.AST;
+
+namespace NuStack.Core.Parse
+{
+    public class Parser
+    {
+        private TokenStream tokenStream;
+
+        public ASTNode Parse(TokenStream tokens)
+        {
+            tokenStream = tokens;
+            return null;
+        }
+    }
+}
