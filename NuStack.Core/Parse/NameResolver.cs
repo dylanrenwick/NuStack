461d62cf5a07ac4e4f8946280862bdb8f84474d2
Dylan Renwick <skidsirongenesisdev@gmail.com>
2021-12-12 01:20:55 -0500

    Added func registration and resolving to NameResolver

--- NuStack.Core/Parse/NameResolver.cs
+++ NuStack.Core/Parse/NameResolver.cs
@@ -2,6 +2,47 @@
 {
     public class NameResolver
     {
+        private List<FuncFingerprint> fingerprints;
 
+        public NameResolver()
+        {
+            fingerprints = new List<FuncFingerprint>();
+        }
+
+        public bool TryResolve(string name, out FuncFingerprint fingerprint)
+        {
+            fingerprint = default;
+
+            var matches = fingerprints.Where(x => x.Name == name);
+            if (matches.Any())
+            {
+                fingerprint = matches.First();
+                return true;
+            }
+
+            return false;
+        }
+
+        public FuncFingerprint RegisterFunctionFingerprint(int tokenStart, int tokenEnd, string name)
+        {
+            var fingerprint = new FuncFingerprint
+            {
+                TokenStart = tokenStart,
+                TokenEnd = tokenEnd,
+                Name = name,
+                InternalName = genInternalFunctionName(name)
+            };
+            return fingerprint;
+        }
+
+        public void Clear()
+        {
+            fingerprints.Clear();
+        }
+
+        private string genInternalFunctionName(string name)
+        {
+            return $"__func_{name}";
+        }
     }
 }
