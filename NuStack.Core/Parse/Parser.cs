acdfd00bb47b8d7db1bd3e1144f3ec5b90f29430
Dylan Renwick <skidsirongenesisdev@gmail.com>
2021-12-12 00:14:16 -0500

    Added pre-parse step to find function definitions

--- NuStack.Core/Parse/Parser.cs
+++ NuStack.Core/Parse/Parser.cs
@@ -12,5 +12,22 @@ namespace NuStack.Core.Parse
             tokenStream = tokens;
             return null;
         }
+
+        private IEnumerable<int> findFuncDefinitions()
+        {
+            var result = new List<int>();
+            tokenStream.EachToken(TokenType.Keyword, "fn");
+            while (tokenStream.MoveNext())
+            {
+                int fnStart = tokenStream.CurrentIndex;
+                Token ident = tokenStream.Expect(TokenType.Identifier);
+                string funcName = ident.Value;
+                tokenStream.Expect(TokenType.OpenParen);
+                tokenStream.Expect(TokenType.CloseParen);
+                result.Add(fnStart);
+            }
+
+            return result;
+        }
     }
 }
