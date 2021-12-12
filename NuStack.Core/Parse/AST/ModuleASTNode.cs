ae31e5486b3b4e66810d656b51f5822504173dc7
Dylan Renwick <skidsirongenesisdev@gmail.com>
2021-12-12 01:36:40 -0500

    Added ModuleASTNode

---
+++ NuStack.Core/Parse/AST/ModuleASTNode.cs
@@ -0,0 +1,19 @@
+﻿namespace NuStack.Core.Parse.AST
+{
+    public class ModuleASTNode : ASTNode
+    {
+        public override IEnumerable<ASTNode> Children => children;
+
+        private List<FuncASTNode> children;
+
+        public override void WriteToStringBuilder(StringBuilder builder)
+        {
+            foreach(var child in Children) child.WriteToStringBuilder(builder);
+        }
+
+        public void AddFunction(FuncASTNode func)
+        {
+            children.Add(func);
+        }
+    }
+}
