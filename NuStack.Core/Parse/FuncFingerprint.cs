0aed08f4993fbf79b6cd95223fe42ec2f4a34988
Dylan Renwick <skidsirongenesisdev@gmail.com>
2021-12-12 01:20:41 -0500

    Added FuncFingerprint

---
+++ NuStack.Core/Parse/FuncFingerprint.cs
@@ -0,0 +1,10 @@
+﻿namespace NuStack.Core.Parse
+{
+    public struct FuncFingerprint
+    {
+        public int TokenStart { get; set; }
+        public int TokenEnd { get; set; }
+        public string Name { get; set; }
+        public string InternalName { get; set; }
+    }
+}
