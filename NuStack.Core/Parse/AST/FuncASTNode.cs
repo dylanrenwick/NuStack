cf4d2fb3820a4225468eb5703faf4ef10550941c
Dylan Renwick <skidsirongenesisdev@gmail.com>
2021-12-12 01:36:32 -0500

    Added FuncASTNode

---
+++ NuStack.Core/Parse/AST/FuncASTNode.cs
@@ -0,0 +1,22 @@
+﻿namespace NuStack.Core.Parse.AST
+{
+    public class FuncASTNode : ASTNode
+    {
+        public FuncFingerprint Fingerprint { get; private set; }
+
+        public override IEnumerable<ASTNode> Children => children;
+
+        private List<ASTNode> children;
+
+        public FuncASTNode(FuncFingerprint fingerprint, IEnumerable<ASTNode> nodes)
+        {
+            Fingerprint = fingerprint;
+            children = nodes.ToList();
+        }
+
+        public override void WriteToStringBuilder(StringBuilder builder)
+        {
+            foreach (var child in Children) child.WriteToStringBuilder(builder);
+        }
+    }
+}
