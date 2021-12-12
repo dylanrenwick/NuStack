6b91906469618db6c18874debd2e72cf396173fa
Dylan Renwick <skidsirongenesisdev@gmail.com>
2021-12-12 01:21:01 -0500

    Added ParserException

---
+++ NuStack.Core/Parse/ParserExceptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NuStack.Core.Parse
+{
+    public class ParserException : Exception
+    {
+        public ParserException(string message)
+            : base(message) { }
+    }
+}
