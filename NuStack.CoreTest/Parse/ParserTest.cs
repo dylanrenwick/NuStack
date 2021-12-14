9a0a430f7431de413e6a46699fa58042080a98e9
Dylan Renwick <skidsirongenesisdev@gmail.com>
2021-12-13 22:38:06 -0500

    Added simple parser test

---
+++ NuStack.CoreTest/Parse/ParserTest.cs
@@ -0,0 +1,23 @@
+﻿using NuStack.Core.Parse;
+using NuStack.Core.Parse.AST;
+using NuStack.Core.Tokens;
+
+namespace NuStack.CoreTest.Parse
+{
+    public class ParserTest
+    {
+        private Parser parser;
+
+        public ParserTest()
+        {
+            parser = new Parser();
+        }
+
+        [Fact]
+        public void Parse_ReturnsModuleASTNode()
+        {
+            var stream = new TokenStream(new Token[0]);
+            Assert.IsType<ModuleASTNode>(parser.Parse(stream));
+        }
+    }
+}
