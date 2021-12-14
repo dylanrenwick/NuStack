20b2058189c5921c56d6774d90fe48b7055fe9d9
Dylan Renwick <skidsirongenesisdev@gmail.com>
2021-12-14 00:34:55 -0500

    Added ReturnASTNode

---
+++ NuStack.Core/Parse/AST/ReturnASTNode.cs
@@ -0,0 +1,18 @@
+﻿using NuStack.Core.Tokens;
+
+namespace NuStack.Core.Parse.AST
+{
+    public class ReturnASTNode : ExpressionASTNode
+    {
+        public ReturnASTNode(ExpressionASTNode expression)
+        {
+            children.Add(expression);
+        }
+
+        public new static ReturnASTNode ParseNode(TokenStream tokens, NameResolver nameResolver)
+        {
+            var expression = ExpressionASTNode.ParseNode(tokens, nameResolver);
+            return new ReturnASTNode(expression);
+        }
+    }
+}
