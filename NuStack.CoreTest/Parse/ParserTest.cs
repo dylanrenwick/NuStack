d9bdf0b99e4582f8b4d2617968e4b2f761d6c9c6
Dylan Renwick <skidsirongenesisdev@gmail.com>
2021-12-13 23:33:10 -0500

    Added full program parser test

--- NuStack.CoreTest/Parse/ParserTest.cs
+++ NuStack.CoreTest/Parse/ParserTest.cs
@@ -19,5 +19,47 @@ namespace NuStack.CoreTest.Parse
             var stream = new TokenStream(new Token[0]);
             Assert.IsType<ModuleASTNode>(parser.Parse(stream));
         }
+
+        public static IEnumerable<object[]> SingleFunction_Data()
+        {
+            var stream = new TokenStream(new List<Token>
+            {
+                new Token(1, 0, 0, TokenType.Keyword, "fn"),
+                new Token(1, 3, 3, TokenType.Identifier, "main"),
+                new Token(1, 7, 7, TokenType.OpenParen),
+                new Token(1, 8, 8, TokenType.CloseParen),
+                new Token(1, 17, 17, TokenType.OpenBrace),
+                new Token(1, 19, 19, TokenType.Keyword, "return"),
+                new Token(1, 26, 26, TokenType.Integer, "0"),
+                new Token(1, 27, 27, TokenType.Semicolon),
+                new Token(1, 29, 29, TokenType.CloseBrace)
+            });
+            var rootNode = new ModuleASTNode();
+            var mainFuncBody = new ExpressionBlockASTNode();
+            var mainFunc = new FuncASTNode(
+                new FuncFingerprint
+                {
+                    TokenStart = 0,
+                    TokenEnd = 4,
+                    Name = "main",
+                    InternalName = "__func_main"
+                }, mainFuncBody);
+            rootNode.AddFunction(mainFunc);
+
+            return new List<object[]>
+            {
+                new object[] { stream, rootNode }
+            };
+        }
+
+        [Theory]
+        [MemberData(nameof(SingleFunction_Data))]
+        public void Parse_SingleFunction_ParsesFunc(
+            TokenStream stream,
+            ModuleASTNode expected
+        )
+        {
+            Assert.Equal(expected, parser.Parse(stream));
+        }
     }
 }
